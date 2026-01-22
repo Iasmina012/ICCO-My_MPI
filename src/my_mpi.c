e331ee094fe068288f352cecf9482bd9c39b22e4
iasmina012 <iasmina.putina012@yahoo.com>
2026-01-22 18:15:53 +0200

    Typo fixed

--- src/my_mpi.c
+++ src/my_mpi.c
@@ -216,7 +216,7 @@ int My_MPI_Scatter(const void *sendbuf, int sendcount, MPI_Datatype sendtype, vo
 
 }
 
-//Gather: naive implementation which is root -> each proces
+//Gather: naive implementation which is each process -> root
 int My_MPI_Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount,  MPI_Datatype recvtype, int root, MPI_Comm comm) {
     
     int rank, size;
